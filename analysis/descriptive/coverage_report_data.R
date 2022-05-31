c2e7982b7bc3305961514331af336376f77dbd88
Millie Green <acagreen17@gmail.com>
2022-05-31 09:41:50 +0100

    fix: typo and rename

--- analysis/descriptive/coverage_report_data.R
+++ analysis/descriptive/coverage_report_data.R
@@ -284,7 +284,7 @@ plot_data_prop_treated <- data_processed_clean %>%
          treat_redacted =  plyr::round_any(treat, 10),
          treat_redacted = ifelse(treat < threshold, NA, treat_redacted),
          prop_redacted = round(treat_redacted/elig_redacted, digits = 4),
-         week = as.Date(week))
+         week = as.Date(week)-2)
 
 write_csv(plot_data_prop_treated %>% 
             select(high_risk_cohort, elig_start = week, prop_redacted) %>% 
